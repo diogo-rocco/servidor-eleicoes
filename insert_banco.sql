24c4eb1b3f8568ae98f3bf60e8d76c40a39a3aaa
diogo-rocco <diogorocco12@gmail.com>
2022-07-29 18:20:52 -0300

    insert de dados

---
+++ insert_banco.sql
@@ -0,0 +1,16 @@
+INSERT INTO candidaturas (IdCandidatura, Cargo, NumeroCandidatura, NomeCandidato, NomeVice, NomePartido, NumeroPartido, FotoCandidato, FotoVice)
+VALUE (12, 'prefeito', '12', 'Finn', 'Jake', 'ADVT', '12', 'https://e7.pngegg.com/pngimages/702/548/png-clipart-adventure-time-finn-the-human-finn-the-human-marceline-the-vampire-queen-jake-the-dog-ice-king-princess-bubblegum-finn-the-human-adventure-time-cartoon-characters-miscellaneous-child.png', 'https://www.nicepng.com/png/detail/872-8728725_adventure-time-jake-the-dog-blinking-jake-adventure.png');
+INSERT INTO candidaturas (IdCandidatura, Cargo, NumeroCandidatura, NomeCandidato, NomeVice, NomePartido, NumeroPartido, FotoCandidato, FotoVice)
+VALUE (13, 'prefeito', '13', 'Steven Universo', 'Connie', 'CRSG', '13', 'https://upload.wikimedia.org/wikipedia/commons/b/b8/Steven_Universo_%28Personagem%29.png', 'https://i.pinimg.com/564x/68/55/c1/6855c18f7a6a0a2b1d3ff223866514c1.jpg');
+INSERT INTO candidaturas (IdCandidatura, Cargo, NumeroCandidatura, NomeCandidato, NomeVice, NomePartido, NumeroPartido, FotoCandidato, FotoVice)
+VALUE (14, 'prefeito', '14', 'Salsicha', 'Scooby Doo', 'SCBD', '14', 'https://i.pinimg.com/236x/29/ca/d5/29cad592e7353217d8d3842dc895eae9--shaggy-from-scooby-doo-shaggy-rogers.jpg', 'https://upload.wikimedia.org/wikipedia/pt/b/b2/Scooby.jpg');
+INSERT INTO candidaturas (IdCandidatura, Cargo, NumeroCandidatura, NomeCandidato, NomeVice, NomePartido, NumeroPartido, FotoCandidato, FotoVice)
+VALUE (15, 'prefeito', '15', 'Batman', 'Robin', 'JSTL', '15', 'https://i.pinimg.com/originals/19/d4/63/19d463bfc2962a1ee07d69efd4936665.png', 'https://i.pinimg.com/originals/7f/5c/b7/7f5cb708bed182055b262f24ab4d60cc.jpg');
+INSERT INTO candidaturas (IdCandidatura, Cargo, NumeroCandidatura, NomeCandidato, NomeVice, NomePartido, NumeroPartido, FotoCandidato, FotoVice)
+VALUE (12123, 'vereador', '12123', 'Princesa Jujuba', '', 'ADVT', '12', 'https://www.pngmart.com/files/13/Adventure-Time-Princess-PNG-HD.png', '');
+INSERT INTO candidaturas (IdCandidatura, Cargo, NumeroCandidatura, NomeCandidato, NomeVice, NomePartido, NumeroPartido, FotoCandidato, FotoVice)
+VALUE (13123, 'vereador', '13123', 'Greg Universo', '', 'CRSG', '13', 'https://i.pinimg.com/736x/29/05/34/290534041f3bd6a09dcae177529dfb4f.jpg', '');
+INSERT INTO candidaturas (IdCandidatura, Cargo, NumeroCandidatura, NomeCandidato, NomeVice, NomePartido, NumeroPartido, FotoCandidato, FotoVice)
+VALUE (14123, 'vereador', '14123', 'Velma', '', 'SCBD', '14', 'https://i.imgur.com/aFEcaJ7.png', '');
+INSERT INTO candidaturas (IdCandidatura, Cargo, NumeroCandidatura, NomeCandidato, NomeVice, NomePartido, NumeroPartido, FotoCandidato, FotoVice)
+VALUE (15123, 'vereador', '15123', 'Vereador descolado', '', 'JSTL', '15', 'https://i.pinimg.com/originals/9f/81/3e/9f813e1257431c069da2dad9cdf6ea84.jpg', '');
